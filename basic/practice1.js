0dd010f4e72def2256a19992858f49a7f3f88a7c
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-01-08 11:06:11 +0530

    08_01_2026

---
+++ basic/practice1.js
@@ -0,0 +1,16 @@
+// 08_01_2026
+
+//write a code to add element at the front of an array
+//  arr=[10,20,30,40,50]
+// add element on th front 5
+//output :[5,10,20,30,40,50]
+// to print console.log()
+
+let arr=[10,20,30,40,50];
+let a=arr.length;
+let b=new Array(a+1);
+b[0]=5;
+for(let i=0;i<a;i++){
+    b[i+1]=arr[i];
+}
+console.log(b);
