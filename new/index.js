7361a8ff130fe22dc720aeb39aa81372ab5de4dd
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-01-21 14:49:17 +0530

    Update index.js

--- new/index.js
+++ new/index.js
@@ -1,20 +1,20 @@
-const {createServer} = require("node:http");
+const http = require("node:http");
 
 const hostname = '127.0.0.1';
 
 const port = 4000;
 
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     if(req.url==="/")
     {
-        res.setHeader('Content-Type','text/plain');
+        res.setHeader('Content-Type','text/html');
         res.write("This is my main page")
     }
     else if(req.url === "/about")
     {
-        res.setHeader('Content-Type','text/plain');
-        res.write("This is my about page")
+        res.setHeader('Content-Type','text/html');
+        res.write("<h1>This is my about page</h1> ")
     }
     else if(req.url === "/contact")
     {
